c5eb1a59e8e8031c8f134297858d2ec880d17483
ankitpalAP <ankitpal@sheerdrive.in>
2021-07-26 22:53:20 +0530

    reducing the latency of api

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { ExchangeRate } from "./components/ExchangeRate";
+import { changeCurrencyCode, getCurrencyCode } from "./store/rate";
 import "./style.css";
+
+//Making the application low latency
+store.dispatch(function getInitialRates(dispatch, getState) {
+  const state = getState();
+  const currencyCode = getCurrencyCode(state);
+  dispatch(changeCurrencyCode(currencyCode));
+});
 
 ReactDOM.render(
   <Provider store={store}>
